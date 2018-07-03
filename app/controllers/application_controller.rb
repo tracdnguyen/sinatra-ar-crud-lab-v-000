58b1c45d7bddc93d611fe034e1bb78facc4098bd
Trac Nguyen <nguyendtrac@gmail.com>
2018-07-03 04:08:24 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,41 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+    erb :index
+  end
 
+  get '/posts/new' do
+    erb :new
+  end
+
+  post '/posts' do
+    @post = Post.create(name: params[:name], content: params[:content])
+    erb :index
+  end
+
+  get '/posts' do
+    erb :index
+  end
+
+  get '/posts/:id' do
+    @post = Post.find(params[:id].to_i)
+    erb :show
+  end
+
+  get '/posts/:id/edit' do
+    @post = Post.find(params[:id].to_i)
+    erb :edit
+  end
+
+  patch '/posts/:id' do
+    @post = Post.find(params[:id].to_i)
+    @post.update(name: params[:name], content: params[:content])
+    erb :show
+  end
+
+  delete '/posts/:id/delete' do
+    @post = Post.find(params[:id].to_i)
+    @post.destroy
+    erb :delete
   end
 end
